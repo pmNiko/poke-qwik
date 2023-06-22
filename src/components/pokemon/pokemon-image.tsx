f8cff1cda0c3ff4da51be3b47117b7307195ae7b
nikoDev <nikolas090189@gmail.com>
2023-06-22 20:34:19 -0300

    Initial commit

---
+++ src/components/pokemon/pokemon-image.tsx
@@ -0,0 +1,42 @@
+import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+interface Props {
+  id: number;
+  size?: number;
+  backImage?: boolean;
+  isVisible?: boolean;
+}
+
+export const PokemonImage = component$(
+  ({ id, size = 200, backImage = false, isVisible = true }: Props) => {
+    const imageLoading = useSignal(true);
+
+    const back = backImage ? "back/" : "";
+
+    useTask$(({ track }) => {
+      track(() => id);
+
+      imageLoading.value = true;
+    });
+
+    return (
+      <div
+        class="flex items-center justify-center "
+        style={{ width: `${size}px`, height: `${size}px` }}
+      >
+        <span class={{ hidden: !imageLoading.value }}>Cargando...</span>
+
+        <img
+          class={[
+            { hidden: imageLoading.value, "brightness-0": isVisible },
+            "transition-all",
+          ]}
+          width={size}
+          height={size}
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${back}${id}.png`}
+          alt="Pokemon sprite"
+          onLoad$={() => (imageLoading.value = false)}
+        />
+      </div>
+    );
+  }
+);
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,11 +1,8 @@
-import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-import type { RequestHandler } from '@builder.io/qwik-city';
+import { component$, Slot, useStyles$ } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
+import Navbar from "~/components/shared/navbar/navbar";
 
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
-
-import styles from './styles.css?inline';
+import styles from "./styles.css?inline";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -18,21 +15,14 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
-export const useServerTimeLoader = routeLoader$(() => {
-  return {
-    date: new Date().toISOString(),
-  };
-});
-
 export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Header />
-      <main>
+      <Navbar />
+      <main class="flex flex-col items-center justify-center">
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
